229aa57cc580b0834662fcbb22d465440f36c86c
Prytula Dmytro Vitaliyovych <dimamon02@gmail.com>
2024-05-10 12:39:25 +0300

    zaglushka added

---
+++ SQLQuery1.sql
@@ -0,0 +1,13 @@
+-- zaglushka
+SELECT * FROM Player;
+SELECT * FROM Nickname;
+SELECT * FROM Character;
+SELECT * FROM Runes;
+SELECT * FROM Items;
+SELECT * FROM Gold;
+SELECT * FROM GoldGained;
+SELECT * FROM PlayerTeam;
+SELECT * FROM Teams;
+SELECT * FROM Matches;
+SELECT * FROM PlayerStats;
+SELECT * FROM MatchParticipants;
